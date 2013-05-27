965d0b89925a91bc49239e8700dedee6cdb83a0f
moodpo <yangxiaoxiehaha@gmail.com>
2013-05-27 19:35:13 +0800

    Update

---
+++ _themes/theme.py
@@ -0,0 +1,21 @@
+#coding: utf-8
+
+
+name = 'moodpo'
+version = '0.10'
+website = 'https://github.com/moodpo/liquidluck-theme-moodpo'
+author = {
+    'name': 'yangxiaoxie',
+    'website': 'http://moodpo.com'
+}
+
+lang = 'zh-CN'
+
+navigation = [
+    {'name': 'Archive', 'link': '/'},
+    {'name': 'Work', 'link': '/work/'},
+    {'name': 'Life', 'link': '/life/'},
+]
+
+
+
